5ec709cabaa82977eb687d2b5f66eeff14a53a9a
sezid <bradsajid@gmail.com>
2024-08-30 22:27:21 +0600

    log update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bodoni: ['Bauer Bodoni Condensed', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
