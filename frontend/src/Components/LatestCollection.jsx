41f66f001ab94fd82f0e3d10f63e7af8ebe91353
sezid <bradsajid@gmail.com>
2024-10-15 19:47:46 +0600

    update minor

--- frontend/src/Components/LatestCollection.jsx
+++ frontend/src/Components/LatestCollection.jsx
@@ -18,7 +18,7 @@ const LatestCollection = () => {
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
             <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
-            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>sdasdsdasdasdadasdasdasdasdasdasdadadadasdasdadasdasdasdasdad</p>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         </div> 
 
     {/* Rendering products */}
