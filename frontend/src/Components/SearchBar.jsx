a57c1494a9443d202178dd0d645b3687fb0d9c4b
sezid <bradsajid@gmail.com>
2024-11-03 19:58:12 +0600

    update

--- frontend/src/Components/SearchBar.jsx
+++ frontend/src/Components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect,useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import { ShopContext } from '../Context/ShopContext'
 import { assets } from '../assets/frontend_assets/assets';
 import { useLocation } from 'react-router-dom';
 
