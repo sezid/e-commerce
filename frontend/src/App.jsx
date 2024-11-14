9e6e3ff8dad0e9b3f80ed0d6413ec70ae88aa38e
sezid <bradsajid@gmail.com>
2024-11-15 00:44:46 +0600

    placeorder pg & cart pg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,14 @@ import Orders from './Pages/Orders'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import SearchBar from './Components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
       <Navbar/>
       <SearchBar/>
       <Routes>
