978fc906142177687c62f36fa1a78b9b9b42dba3
sezid <bradsajid@gmail.com>
2024-11-15 20:17:34 +0600

    complete frontend

--- frontend/src/Components/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter.jsx
@@ -10,7 +10,7 @@ const NewsLetter = () => {
     <div className='text-center'>
       <p className='text-2xl font-medium text-gray-800'>Subscribe Now to Get 20% OFF</p>
       <p className='text-gray-400 mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-      <form onSubmit={onSubmitHandler} className='w-full sm:1-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+      <form onSubmit={onSubmitHandler} className='w-1/2 sm:1-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Your Email' required/>
         <button className='bg-black text-white text-xs px-10 py-4' type='submit'>SUBSCRIBE</button>
       </form>
--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import Title from '../Components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import NewsLetter from '../Components/NewsLetter'
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+            <p>E-commerce (electronic commerce) is the activity of electronically buying or selling products and services on online platforms or over the Internet. E-commerce draws on technologies such as mobile commerce, electronic funds transfer, supply chain management, Internet marketing, online transaction processing, electronic data interchange (EDI), inventory management systems, and automated data collection systems. E-commerce is the largest sector of the electronics industry and is in turn driven by the technological advances of the semiconductor industry.</p>
+            <p>E-commerce typically uses the web for at least a part of a transaction's life cycle although it may also use other technologies such as e-mail. Typical e-commerce transactions include the purchase of products (such as books from Amazon) or services (such as music downloads in the form of digital distribution such as the iTunes Store). There are three areas of e-commerce: online retailing, electronic markets, and online auctions.</p>
+            <b className='text-gray-800'>Our Mission</b>
+            <p>Our mission at Dapper is to empower customers with choices, convinience, and confidence. We're dedicated to providing seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>          
+          </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convinience:</b>
+          <p className='text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div> 
+      <NewsLetter/>
+
     </div>
   )
 }
