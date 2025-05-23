9b303caa9480c43bdc9705992ae0894e70103cb1
sezid <bradsajid@gmail.com>
2025-05-23 23:04:22 +0600

    backend development started

---
+++ backend/config/mongodb.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const connectDb = async ()=>{
+    mongoose.connection.on('connected', ()=>{
+        console.log("DB CONNECTED");
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+}
+
+export default connectDb;
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import connectDb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+
+//App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDb()
+connectCloudinary()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.use('/api/user',userRouter)
+
+app.get('/',(req, res)=>{
+    res.send("API Working")
+})
+
+
+app.listen(port,() => console.log("Server Started on port: "+port))
